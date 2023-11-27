5094fd8cbf3dcb99e86f5d1eff308db99920591c
alexlam184 <alexwork0211@gmail.com>
2023-11-26 20:30:08 -0800

    lint: add format style

--- src/components/home/hero/DotGrid.tsx
+++ src/components/home/hero/DotGrid.tsx
@@ -1,5 +1,5 @@
 import styles from "./dotgrid.module.scss";
-import anime from "animejs";
+// import anime from "animejs";
 
 export const DotGrid = () => {
   const GRID_WIDTH = 25;
@@ -8,25 +8,25 @@ export const DotGrid = () => {
   const dots = [];
 
   const handleDotClick = (e: any) => {
-    anime({
-      targets: ".dot-point",
-      scale: [
-        { value: 1.35, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      translateY: [
-        { value: -15, easing: "easeOutSine", duration: 250 },
-        { value: 1, easing: "easeInOutQuad", duration: 500 },
-      ],
-      opacity: [
-        { value: 0.7, easing: "easeOutSine", duration: 250 },
-        { value: 0.35, easing: "easeInOutQuad", duration: 500 },
-      ],
-      delay: anime.stagger(100, {
-        grid: [GRID_WIDTH, GRID_HEIGHT],
-        from: e.target.dataset.index,
-      }),
-    });
+    // anime({
+    //   targets: ".dot-point",
+    //   scale: [
+    //     { value: 1.35, easing: "easeOutSine", duration: 250 },
+    //     { value: 1, easing: "easeInOutQuad", duration: 500 },
+    //   ],
+    //   translateY: [
+    //     { value: -15, easing: "easeOutSine", duration: 250 },
+    //     { value: 1, easing: "easeInOutQuad", duration: 500 },
+    //   ],
+    //   opacity: [
+    //     { value: 0.7, easing: "easeOutSine", duration: 250 },
+    //     { value: 0.35, easing: "easeInOutQuad", duration: 500 },
+    //   ],
+    //   delay: anime.stagger(100, {
+    //     grid: [GRID_WIDTH, GRID_HEIGHT],
+    //     from: e.target.dataset.index,
+    //   }),
+    // });
   };
 
   let index = 0;
